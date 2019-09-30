bb132024763367cabb4a24d1ee80fde8a3ef0981
dades <dadesantis96@yahoo.com>
2019-09-30 16:25:19 -0400

    Some clean up

--- challenge35/RGBswap.py
+++ challenge35/RGBswap.py
@@ -1,30 +1,30 @@
-###
-# Coding Challenge #35
-# 9/30/19
-###
-
 import time as t
 
 
+# Organizes the characters in the array using swaps, such
+# that they end up in the order RGB
 def swap(letters):
+    # Keep track of the R and G indexes
     r_index = 0
-    g_index = 0
 
+    # First, place all the Rs in the beginning
+    # at the appropriate index. Keep track of the last r_index.
     for i in range(0, len(letters)):
-        letters[i].capitalize()
         if letters[i] is 'R':
             letters[i], letters[r_index] = letters[r_index], letters[i]
             if r_index + 1 < len(letters):
                 r_index += 1
-                g_index = r_index + 1
-                # print("i: {} \nr_index: {} \ng_index: {}".format(i, r_index, g_index))
 
-    for i in range(r_index+1, len(letters)):
+    # Next, place all Gs after the Rs. Pick up at the index after
+    # the last r_index.
+    g_index = r_index + 1
+    for i in range(g_index, len(letters)):
         if letters[i] is 'G':
             letters[i], letters[g_index] = letters[g_index], letters[i]
             if g_index + 1 < len(letters):
                 g_index += 1
 
+    # The Bs have been sorted as a result of the other swaps, so return the sorted array.
     return letters
 
 
@@ -33,10 +33,12 @@ ex1 = ['G', 'G', 'G', 'B', 'B', 'R', 'R', 'G', 'G', 'B']
 
 print("ex before", ex)
 start = t.perf_counter()
-print("ex after", swap(ex))
+swap(ex)
 print('ex time: {}s'.format(t.perf_counter() - start))
+print("ex after", ex)
 
 print("ex1 before", ex1)
 start = t.perf_counter()
-print("ex1 after", swap(ex1))
+swap(ex1)
 print('ex1 time: {}s'.format(t.perf_counter() - start))
+print("ex1 after", ex1)
